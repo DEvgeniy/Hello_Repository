cddc065cb8b79c6c4e8af20db4dddbaece8df493
DEvgeniy <ederbanov@bk.ru>
2016-07-05 09:51:19 +0300

    Add files via upload

---
+++ MyBoolean.java
@@ -0,0 +1,13 @@
+public class MyBoolean{
+
+public static void main (String[] args){
+
+  boolean a = true;
+  boolean b = false;
+  System.out.println("\"NOT\" (!) : !b = "+(!b));		// For showing in text line symbo " we need use \" 
+  System.out.println("\"AND\" (&) : a&b = "+(a&b));
+  System.out.println("\"OR\" (|) : a|b = "+(a|b));
+  System.out.println("\"XOR\" (^) : ^b = "+(a^b));
+
+  }
+}
---
+++ MySecond.java
@@ -0,0 +1,8 @@
+// My second programm on Java
+
+class MySecond {
+
+public static void main (String [] args) {
+  System.out.print("Hello World!");
+  }
+}
